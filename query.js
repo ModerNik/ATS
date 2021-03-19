0d3f8355ce72d57308c83b2413b81f7f90073390
stata09 <77682038+stata09@users.noreply.github.com>
2021-03-19 14:53:23 +0300

    Add files via upload

---
+++ query.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+  $("#btn").click(
+  function(){
+    $.ajax({
+      url: 'action2.php', //url страницы (action_ajax_form.php)
+      type: 'POST', //метод отправки
+      dataType: 'html', //формат данных
+      data: $('#ajax_form').serialize(),  // Сеарилизуем объект
+      success: function(response) { //Данные отправлены успешно
+        result = $.parseJSON(response);
+        $('#result_form').html('YES');
+      },
+      error: function(response) { // Данные не отправлены
+        $('#result_form').html('Ошибка. Данные не отправлены.');
+      }
+   });
+   return false; 
+  }
+);
+});
