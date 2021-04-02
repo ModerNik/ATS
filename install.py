4de06c15dec046ef7c392bbe321617abf465c4f7
stata09 <77682038+stata09@users.noreply.github.com>
2021-04-02 09:48:41 +0300

    Update install.py

--- install.py
+++ install.py
@@ -5,3 +5,17 @@ r = requests.get('https://github.com/ModerNik/ATS/blob/main/test.zip?raw=true')
 
 with r, zipfile.ZipFile(io.BytesIO(r.content)) as archive:
     archive.extractall()
+url = 'https://raw.githubusercontent.com/ModerNik/ATS/main/server.py'  
+r = requests.get(url)
+
+print(r.status_code)  
+print(r.headers['content-type'])  
+print(r.encoding)
+
+ok = r.text
+
+file = open("server.py", "w", encoding='utf-8')
+file.write(ok)
+file.close()
+
+print('DONE')
