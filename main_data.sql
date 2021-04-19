71c88013c8572c5a840788d039642d4481eeb587
stata09 <77682038+stata09@users.noreply.github.com>
2021-04-19 20:21:55 +0300

    Add files via upload

--- main_data.sql
+++ main_data.sql
@@ -33,7 +33,30 @@ CREATE TABLE `11a` (
 
 LOCK TABLES `11a` WRITE;
 /*!40000 ALTER TABLE `11a` DISABLE KEYS */;
+INSERT INTO `11a` VALUES ('Maksim Kolechko'),('Dmitriy Bobs'),('Artem Aretkman');
 /*!40000 ALTER TABLE `11a` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `11b`
+--
+
+DROP TABLE IF EXISTS `11b`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `11b` (
+  `name` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `11b`
+--
+
+LOCK TABLES `11b` WRITE;
+/*!40000 ALTER TABLE `11b` DISABLE KEYS */;
+INSERT INTO `11b` VALUES ('Pavel Volya');
+/*!40000 ALTER TABLE `11b` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -54,7 +77,7 @@ CREATE TABLE `groups` (
 
 LOCK TABLES `groups` WRITE;
 /*!40000 ALTER TABLE `groups` DISABLE KEYS */;
-INSERT INTO `groups` VALUES ('11В');
+INSERT INTO `groups` VALUES ('11A');
 /*!40000 ALTER TABLE `groups` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -79,7 +102,7 @@ CREATE TABLE `logpass` (
 
 LOCK TABLES `logpass` WRITE;
 /*!40000 ALTER TABLE `logpass` DISABLE KEYS */;
-INSERT INTO `logpass` VALUES ('admin','admin','admin',NULL),('Oleg','12345','teacher',NULL),('Stas','54321','ladmin',NULL),('Kate','1111','student',NULL);
+INSERT INTO `logpass` VALUES ('admin','admin','admin',NULL),('Stas','54321','ladmin',NULL),('Kate','1111','student',NULL),('Oleg','12345','teacher','Oleg Detinkin');
 /*!40000 ALTER TABLE `logpass` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -103,6 +126,7 @@ CREATE TABLE `plans` (
 
 LOCK TABLES `plans` WRITE;
 /*!40000 ALTER TABLE `plans` DISABLE KEYS */;
+INSERT INTO `plans` VALUES (3,'11a','2021-04-07');
 /*!40000 ALTER TABLE `plans` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -126,7 +150,7 @@ CREATE TABLE `questions` (
   `answer` int(11) DEFAULT NULL,
   `teacher` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -135,6 +159,7 @@ CREATE TABLE `questions` (
 
 LOCK TABLES `questions` WRITE;
 /*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+INSERT INTO `questions` VALUES (1,'INF','numeral systems','11','perevedite 2021 v 8 systemu schislenia','23044','3745','3965','5615',2,'Oleg Detinkin'),(2,'INF','numeral systems','11','perevedite 1100101101 iz 2 v 10 systemu schislenia','813','6','234','318',1,'Oleg Detinkin'),(3,'INF','numeral systems','11','skolko edinic soderjit droichnaya zapis chisla 1234','8','11','3','5',4,'Oleg Detinkin'),(4,'INF','numeral systems','11','perevedite chislo 1234 iz 8 v 16 systemu','29C','1234','1‘3','294',1,'Oleg Detinkin'),(5,'INF','numeral systems','11','perevedite 11111111 iz 2 v 10 systemu schislenia','127','111','255','100',3,'Oleg Detinkin'),(6,'INF','numeral systems','11','123 v 10 + 123 v 8. Otvet daite v 10 systeme','246','206','194','216',2,'Oleg Detinkin'),(7,'INF','numeral systems','11','735 v 8 + 304 v 8. Otvet daite v 8 systeme','403','431','444','48',2,'Oleg Detinkin');
 /*!40000 ALTER TABLE `questions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -172,7 +197,7 @@ DROP TABLE IF EXISTS `teachers`;
 CREATE TABLE `teachers` (
   `name` varchar(50) DEFAULT NULL,
   `sub` varchar(3) DEFAULT NULL,
-  `groups` set('11Ђ','11Ѓ','11‚') DEFAULT NULL
+  `groups` set('9A','9B','9C','10A','10B','10C','11A','11B','11C') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -182,7 +207,7 @@ CREATE TABLE `teachers` (
 
 LOCK TABLES `teachers` WRITE;
 /*!40000 ALTER TABLE `teachers` DISABLE KEYS */;
-INSERT INTO `teachers` VALUES ('Stas Shimchenko','MA','11Ђ,11Ѓ'),('Oleg Detinkin','INF','11‚');
+INSERT INTO `teachers` VALUES ('Oleg Detinkin','INF','11A,11C'),('Stas Shimchenko','MA','11B');
 /*!40000 ALTER TABLE `teachers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -201,7 +226,7 @@ CREATE TABLE `tests` (
   `num` set('1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60') DEFAULT NULL,
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -210,6 +235,7 @@ CREATE TABLE `tests` (
 
 LOCK TABLES `tests` WRITE;
 /*!40000 ALTER TABLE `tests` DISABLE KEYS */;
+INSERT INTO `tests` VALUES ('INF','numeral systems','11','Oleg Detinkin','1,2,3,4,5,6,7',1);
 /*!40000 ALTER TABLE `tests` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -222,4 +248,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-02-19 12:41:46
+-- Dump completed on 2021-04-19 20:18:57
